c462f8e9c0b1e3a66677580c9d7eb2ca063834f6
Gustavo Canete <gcanete@netsuite.com>
2015-10-21 17:14:43 -0300

    initial commit

---
+++ www/app/app.module.js
@@ -0,0 +1,33 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+(function() {
+    'use strict';
+
+    angular
+        .module('moviesApp', [
+            'moviesApp.core',
+            'ionic'
+        ])
+        .run(moviesAppRun);
+
+    /* @ngInject */
+    function moviesAppRun($ionicPlatform) {
+
+        $ionicPlatform.ready(ionicPlatformReady);
+
+        function ionicPlatformReady() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        }
+    }
+
+})();
---
+++ www/app/core/core.config.js
@@ -0,0 +1,34 @@
+(function myAppCoreConfig() {
+    'use strict';
+
+    angular
+        .module('moviesApp.core')
+        .config(coreConfig)
+        .config(themeConfig);
+
+    coreConfig.$inject = [
+        '$urlRouterProvider'
+    ];
+
+    /* @ngInject */
+    function coreConfig($urlRouterProvider) {
+        $urlRouterProvider.otherwise('/login');
+    }
+
+    /* @ngInject */
+    function themeConfig($mdThemingProvider) {
+
+        $mdThemingProvider.theme('default')
+            .dark()
+            .primaryPalette('grey', {
+                'default': '900'
+            })
+            .accentPalette('deep-purple', {
+                'default': '500'
+            })
+            .warnPalette('red', {
+                'default': '300'
+            });
+    }
+
+})();
